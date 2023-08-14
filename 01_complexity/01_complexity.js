68296556edc42dfa393c924b776d38b916efe855
Trai Lynne Compton <trailynne@gmail.com>
2023-08-14 21:27:27 +0000

    Answer `makeCombinedMatrix`

--- 01_complexity/01_complexity.js
+++ 01_complexity/01_complexity.js
@@ -276,8 +276,8 @@ function printUnorderedPairs(array) {
 /*
  *  Make Combined Matrix
  *
- *  Time Complexity:
- *  Auxiliary Space Complexity:
+ *  Time Complexity: O(n * m)
+ *  Auxiliary Space Complexity: O(n + m)
  */
 
 function makeCombinedMatrix(arr1, arr2) {
